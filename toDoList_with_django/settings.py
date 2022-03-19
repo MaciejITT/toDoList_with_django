a71acafc0f075041ca7b8b1ba78185c70bfb00c2
Maciej Wadas <53495687+MaciejITT@users.noreply.github.com>
2022-03-19 13:02:44 +0100

    Update settings.py

--- toDoList_with_django/settings.py
+++ toDoList_with_django/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5t%=bg7g+a7xi^ak2-uvgiiu9ycrt^p$if(3wu4(l^ympp)yyq'
+SECRET_KEY = 'hidden'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
